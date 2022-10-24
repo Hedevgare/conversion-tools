3e6f564d9b2b02e125d5dcd5b35feb130c451f58
Edgar Moreira <edgar.dev.moreira@gmail.com>
2022-10-24 21:34:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Binary to Decimal
+
+*Binary to Decimal* is a web app that allows the user to enter strings of 0's and 1's, in any sequence and then displays its decimal equivalent.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  data() {
+    return {
+      binary: "",
+      result: "Enter your binary number",
+    };
+  },
+  methods: {
+    validateInput() {
+      const regex = new RegExp("^[01]+$", "g");
+      if (!this.binary) {
+        this.result = "Enter your binary number";
+      } else if (regex.test(this.binary)) {
+        this.convertBinary();
+      } else {
+        this.result = "Invalid input!";
+      }
+    },
+    convertBinary() {
+      this.result = this.binary
+        .split("")
+        .map((n) => parseInt(n))
+        .reverse()
+        .reduce(
+          (total, currentBinary, index) =>
+            total + currentBinary * Math.pow(2, index)
+        );
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Bin2Dec</h1>
+  <input v-model="binary" @input="validateInput" />
+  <p>{{ result }}</p>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 60px;
+  margin-bottom: 65px;
+}
+
+p {
+  font-size: 40px;
+}
+
+input {
+  border-radius: 5px;
+  height: 50px;
+  font-size: 35px;
+  text-align: center;
+}
+</style>
