4c61ac0d98634326c7818805d404eda22ed28d55
Edgar Moreira <hedegaremoreira@gmail.com>
2024-04-04 19:16:36 +0100

    Add vue-router and setup router

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,8 @@
-<script>
-import NumberConverter from "./components/NumberConverter.vue";
-import LengthConverter from "./components/LengthConverter.vue"
-
-export default {
-  components: {
-    NumberConverter,
-    LengthConverter
-  },
-  data() {
-    return {
-      type: "number"
-    };
-  },
-  methods: {
-    changeType(type) {
-      this.type = type;
-    }
-  }
-}
-</script>
-
 <template>
   <div class="header">
-    <p class="header-link" @click="changeType('number')">Number Converter</p>
-    <p class="header-link" @click="changeType('length')">Length Converter</p>
+    <RouterLink class="header-link" to="/">Number Converter</RouterLink>
+    <RouterLink class="header-link" to="/length">Length Converter</RouterLink>
   </div>
   <hr />
-  <NumberConverter v-if="type == 'number'" />
-  <LengthConverter v-if="type == 'length'" />
+  <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import { createApp } from "vue";
+import { createMemoryHistory, createRouter } from "vue-router";
 import App from "./App.vue";
 
 import "./styles/main.css";
 
-createApp(App).mount("#app");
+import LengthConverter from "./components/LengthConverter.vue";
+import NumberConverter from "./components/NumberConverter.vue";
+
+const routes = [
+    { path: '/', component: NumberConverter },
+    { path: '/length', component: LengthConverter }
+];
+
+const router = createRouter({
+    linkActiveClass: "underline-text",
+    history: createMemoryHistory(),
+    routes
+});
+
+createApp(App).use(router).mount("#app");
