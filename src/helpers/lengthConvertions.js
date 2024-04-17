eea0e24f3b48d0a4b01e8d88eb2fe0faa3c5051b
Edgar Moreira <hedegaremoreira@gmail.com>
2024-04-17 18:02:42 +0100

    Fix return values decimal places

--- src/helpers/lengthConvertions.js
+++ src/helpers/lengthConvertions.js
@@ -2,12 +2,12 @@
  * Meters convertions
  */
 export function feetToMeters(feet) {
-    return feet * 0.3048;
+    return (feet * 0.3048).toFixed(4);
 }
 
 /**
  * Feet convertions
  */
 export function metersToFeet(meters) {
-    return meters * 3.2808;
+    return (meters * 3.2808).toFixed(4);
 }
