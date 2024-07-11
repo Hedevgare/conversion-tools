91c5d539cf2d7bc4afaf9f85409712b6c657d343
Edgar Moreira <hedegaremoreira@gmail.com>
2024-07-12 00:56:02 +0100

    chore: Reorganize test functions
    This commit reorganizes the test functions for number and length
    conversions.

--- tests/helpers/lengthConversions.test.js
+++ tests/helpers/lengthConversions.test.js
@@ -6,47 +6,37 @@ import { startConvertion } from '../../src/helpers/conversions';
  */
 
 // Meters to feet
-test('Meters to Feet: 17 => 55.7742782152', () => {
+test('Meters to Feet', () => {
     expect(startConvertion('Meters', 'Feet', 17, true)).toBe(55.7742782152);
-});
-test('Meters to Feet: 0 => 0', () => {
     expect(startConvertion('Meters', 'Feet', 0, true)).toBe(0);
 });
 // Inches to feet
-test('Inches to Feet: 17 => 1.4166666667', () => { 
+test('Inches to Feet', () => { 
     expect(startConvertion('Inches', 'Feet', 17, true)).toBe(1.4166666667);
-});
-test('Inches to Feet: 0 => 0', () => {
     expect(startConvertion('Inches', 'Feet', 0, true)).toBe(0);
 });
 // Centimeters to feet
-test('Centimeters to Feet: 23 => 0.7545931759' , () => {
+test('Centimeters to Feet' , () => {
     expect(startConvertion('Centimeters', 'Feet', 23, true)).toBe(0.7545931759);
-});
-test('Centimeters to Feet: 0 => 0', () => {
     expect(startConvertion('Centimeters', 'Feet', 0, true)).toBe(0);
 });
 // Millimeters to feet
-test('Millimeters to Feet: 5 => 0.0164041995', () => {
+test('Millimeters to Feet', () => {
     expect(startConvertion('Millimeters', 'Feet', 5, true)).toBe(0.0164041995);
-});
-test('Millimeters to Feet: 0 => 0', () => {
     expect(startConvertion('Millimeters', 'Feet', 0, true)).toBe(0);
 });
 // Kilometers to feet
-test('Kilometers to Feet: 0.7 => 2296.5879265092', () => {
+test('Kilometers to Feet', () => {
     expect(startConvertion('Kilometers', 'Feet', 0.7, true)).toBe(2296.5879265092);
-});
-test('Kilometers to Feet: 0 => 0', () => {
     expect(startConvertion('Kilometers', 'Feet', 0, true)).toBe(0);
 });
 
 /**
  * Test invalid inputs
  */
-test('Test for negative numbers: -1 => Invalid input!', () => {
+test('Negative Numbers', () => {
     expect(startConvertion('Meters', 'Feet', -1)).toBe('Invalid input!');
 });
-test('Test for text characters: test => Invalid input!', () => {
+test('Input with Text Characters', () => {
     expect(startConvertion('Meters', 'Feet', 'test')).toBe('Invalid input!');
 });
--- tests/helpers/numberConversions.test.js
+++ tests/helpers/numberConversions.test.js
@@ -6,33 +6,21 @@ import { startConvertion } from '../../src/helpers/conversions';
  */
 
 // Octal to binary
-test('Octal to Binary: 34 => 11100', () => {
+test('Octal to Binary', () => {
     expect(startConvertion('Octal', 'Binary', '34')).toBe('11100');
-});
-test('Octal to Binary: -21 => -10001', () => {
     expect(startConvertion('Octal', 'Binary', '-21')).toBe('-10001');
-});
-test('Octal to Binary: 0 => 0', () => {
     expect(startConvertion('Octal', 'Binary', '0')).toBe('0');
 });
 // Decimal to binary
-test('Decimal to Binary: 73 => 1001001', () => {
+test('Decimal to Binary', () => {
     expect(startConvertion('Decimal', 'Binary', '73')).toBe('1001001');
-});
-test('Decimal to Binary: -31 => -11111', () => {
     expect(startConvertion('Decimal', 'Binary', '-31')).toBe('-11111');
-});
-test('Decimal to Binary: 0 => 0', () => {
     expect(startConvertion('Decimal', 'Binary', '0')).toBe('0');
 });
 // Hexadecimal to binary
-test('Hexadecimal to Binary: 1A => 11010', () => {
+test('Hexadecimal to Binary', () => {
     expect(startConvertion('Hexadecimal', 'Binary', '1A')).toBe('11010');
-});
-test('Hexadecimal to Binary: -FA => -11111010', () => {
     expect(startConvertion('Hexadecimal', 'Binary', '-FA')).toBe('-11111010');
-});
-test('Hexadecimal to Binary: 0 => 0', () => {
     expect(startConvertion('Hexadecimal', 'Binary', '0')).toBe('0');
 });
 
@@ -41,33 +29,21 @@ test('Hexadecimal to Binary: 0 => 0', () => {
  */
 
 // Binary to octal
-test('Binary to Octal: 11100 => 34', () => {
+test('Binary to Octal', () => {
     expect(startConvertion('Binary', 'Octal', '11100')).toBe('34');
-});
-test('Binary to Octal: -10001 => -21', () => {
     expect(startConvertion('Binary', 'Octal', '-10001')).toBe('-21');
-});
-test('Binary to Octal: 0 => 0', () => {
     expect(startConvertion('Binary', 'Octal', '0')).toBe('0');
 });
 // Decimal to octal
-test('Decimal to Octal: 73 => 111', () => {
+test('Decimal to Octal', () => {
     expect(startConvertion('Decimal', 'Octal', '73')).toBe('111');
-});
-test('Decimal to Octal: -31 => -37', () => {
     expect(startConvertion('Decimal', 'Octal', '-31')).toBe('-37');
-});
-test('Decimal to Octal: 0 => 0', () => {
     expect(startConvertion('Decimal', 'Octal', '0')).toBe('0');
 });
 // Hexadecimal to octal
-test('Hexadecimal to Octal: 1A => 32', () => {
+test('Hexadecimal to Octal', () => {
     expect(startConvertion('Hexadecimal', 'Octal', '1A')).toBe('32');
-});
-test('Hexadecimal to Octal: -FA => -372', () => {
     expect(startConvertion('Hexadecimal', 'Octal', '-FA')).toBe('-372');
-});
-test('Hexadecimal to Octal: 0 => 0', () => {
     expect(startConvertion('Hexadecimal', 'Octal', '0')).toBe('0');
 });
 
@@ -76,33 +52,21 @@ test('Hexadecimal to Octal: 0 => 0', () => {
  */
 
 // Binary to decimal
-test('Binary to Decimal: 11100 => 28', () => {
+test('Binary to Decimal', () => {
     expect(startConvertion('Binary', 'Decimal', '11100')).toBe('28');
-});
-test('Binary to Decimal: -10001 => -17', () => {
     expect(startConvertion('Binary', 'Decimal', '-10001')).toBe('-17');
-});
-test('Binary to Decimal: 0 => 0', () => {
     expect(startConvertion('Binary', 'Decimal', '0')).toBe('0');
 });
 // Octal to decimal
-test('Octal to Decimal: 34 => 28', () => {
+test('Octal to Decimal', () => {
     expect(startConvertion('Octal', 'Decimal', '34')).toBe('28');
-});
-test('Octal to Decimal: -21 => -17', () => {
     expect(startConvertion('Octal', 'Decimal', '-21')).toBe('-17');
-});
-test('Octal to Decimal: 0 => 0', () => {
     expect(startConvertion('Octal', 'Decimal', '0')).toBe('0');
 });
 // Hexadecimal to decimal
-test('Hexadecimal to Decimal: 1A => 26', () => {
+test('Hexadecimal to Decimal', () => {
     expect(startConvertion('Hexadecimal', 'Decimal', '1A')).toBe('26');
-});
-test('Hexadecimal to Decimal: -FA => -250', () => {
     expect(startConvertion('Hexadecimal', 'Decimal', '-FA')).toBe('-250');
-});
-test('Hexadecimal to Decimal: 0 => 0', () => {
     expect(startConvertion('Hexadecimal', 'Decimal', '0')).toBe('0');
 });
 
@@ -111,33 +75,21 @@ test('Hexadecimal to Decimal: 0 => 0', () => {
  */
 
 // Binary to hexadecimal
-test('Binary to Hexadecimal: 11100 => 1C', () => {
+test('Binary to Hexadecimal', () => {
     expect(startConvertion('Binary', 'Hexadecimal', '11100')).toBe('1C');
-});
-test('Binary to Hexadecimal: -10001 => -11', () => {
     expect(startConvertion('Binary', 'Hexadecimal', '-10001')).toBe('-11');
-});
-test('Binary to Hexadecimal: 0 => 0', () => {
     expect(startConvertion('Binary', 'Hexadecimal', '0')).toBe('0');
 });
 // Octal to hexadecimal
-test('Octal to Hexadecimal: 34 => 1C', () => {
+test('Octal to Hexadecimal', () => {
     expect(startConvertion('Octal', 'Hexadecimal', '34')).toBe('1C');
-});
-test('Octal to Hexadecimal: -21 => -11', () => {
     expect(startConvertion('Octal', 'Hexadecimal', '-21')).toBe('-11');
-});
-test('Octal to Hexadecimal: 0 => 0', () => {
     expect(startConvertion('Octal', 'Hexadecimal', '0')).toBe('0');
 });
 // Decimal to hexadecimal
-test('Decimal to Hexadecimal: 73 => 49', () => {
+test('Decimal to Hexadecimal', () => {
     expect(startConvertion('Decimal', 'Hexadecimal', '73')).toBe('49');
-});
-test('Decimal to Hexadecimal: -31 => -1F', () => {
     expect(startConvertion('Decimal', 'Hexadecimal', '-31')).toBe('-1F');
-});
-test('Decimal to Hexadecimal: 0 => 0', () => {
     expect(startConvertion('Decimal', 'Hexadecimal', '0')).toBe('0');
 });
 
@@ -145,15 +97,15 @@ test('Decimal to Hexadecimal: 0 => 0', () => {
  * Test invalid conversions
  */
 
-test('Binary to Decimal: 2 => Invalid input!', () => {
-    expect(startConvertion('Binary','Decimal','2')).toBe('Invalid input!');
+test('Invalid Binary', () => {
+    expect(startConvertion('Binary', 'Decimal', '2')).toBe('Invalid input!');
 });
-test('Octal to Decimal: 8 => Invalid input!', () => {
+test('Invalid Octal', () => {
     expect(startConvertion('Octal', 'Decimal', '8')).toBe('Invalid input!');
 });
-test('Decimal to Binary: 2.5 => Invalid input!', () => {
-    expect(startConvertion('Decimal','Binary','2.5')).toBe('Invalid input!');
+test('Invalid Decimal', () => {
+    expect(startConvertion('Decimal', 'Binary', '2.5')).toBe('Invalid input!');
 });
-test('Hexadecimal to Decimal: 1G => Invalid input!', () => {
+test('Invalid Hexadecimal', () => {
     expect(startConvertion('Hexadecimal', 'Decimal', '1G')).toBe('Invalid input!');
 });
