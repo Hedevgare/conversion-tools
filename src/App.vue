e1fa7d7ff5d8fa1c7a30a6f331117ebbeecc8d01
Edgar Moreira <hedegaremoreira@gmail.com>
2024-11-23 17:59:57 +0000

    refactor: Change menu for mobile
    This commit changes the menu css styles to adapt it to small screens;it
    also corrects the 'main.css' file identation.

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,81 @@
 <script>
 export default {
-	data() {
-		return {
-			appVersion: 'v0.6.0',
-			isMenuOpen: false,
-		}
-	},
-	computed: {
-		footer() {
-			return `Hedegare ${new Date().getFullYear()} - ${this.appVersion}${import.meta.env.PROD ? '' : '(DEVELOPMENT)'}`
-		}
-	},
-	methods: {
-		toggleMenu() {
-			this.isMenuOpen = !this.isMenuOpen;
-		}
-	}
+    data() {
+        return {
+            appVersion: 'v0.6.1',
+            isMenuOpen: false,
+        }
+    },
+    computed: {
+        footer() {
+            return `Hedegare ${new Date().getFullYear()} - ${this.appVersion}${import.meta.env.PROD ? '' : '(DEVELOPMENT)'}`
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+        }
+    }
 }
 </script>
 
 <template>
-	<div class="header">
-		<div class="hamburger-button" :class="{ 'open': isMenuOpen }" @click="toggleMenu">
-			<div class="bar-1"></div>
-			<div class="bar-2"></div>
-			<div class="bar-3"></div>
-		</div>
-	</div>
-	<hr />
-	<section class="menu" :class="{ 'open': isMenuOpen }">
-		<div class="menu-container">
-			<div class="menu-item">
-				<RouterLink class="header-link" to="/" @click="toggleMenu">
-					<font-awesome-icon :icon="['fa', 'hashtag']" />
-					Numbers
-				</RouterLink>
-			</div>
-			<div class="menu-item">
-				<RouterLink class="header-link" to="/length" @click="toggleMenu">
-					<font-awesome-icon :icon="['fa', 'ruler']" />
-					Lengths
-				</RouterLink>
-			</div>
-			<div class="menu-item">
-				<RouterLink class="header-link" to="/temperature" @click="toggleMenu">
-					<font-awesome-icon :icon="['fa', 'temperature-half']" />
-					Temperatures
-				</RouterLink>
-			</div>
-			<div class="menu-item">
-				<RouterLink class="header-link" to="/volume" @click="toggleMenu">
-					<font-awesome-icon :icon="['fa', 'flask']" />
-					Volumes
-				</RouterLink>
-			</div>
-			<div class="menu-item">
-				<RouterLink class="header-link" to="/weight" @click="toggleMenu">
-					<font-awesome-icon :icon="['fa', 'weight-hanging']" />
-					Weights
-				</RouterLink>
-			</div>
-			<div class="menu-item">
-				<RouterLink class="header-link" to="/time" @click="toggleMenu">
-					<font-awesome-icon :icon="['fa', 'hourglass']" />
-					Times
-				</RouterLink>
-			</div>
-			<div class="menu-item">
-				<RouterLink class="header-link" to="/area" @click="toggleMenu">
-					<font-awesome-icon :icon="['fa', 'arrows-left-right-to-line']" />
-					Areas
-				</RouterLink>
-			</div>
-		</div>
-	</section>
-	<RouterView />
-	<div class="footer">
-		<p>{{ footer }}</p>
-	</div>
+    <div class="header">
+        <div class="hamburger-button" :class="{ 'open': isMenuOpen }" @click="toggleMenu">
+            <div class="bar-1"></div>
+            <div class="bar-2"></div>
+            <div class="bar-3"></div>
+        </div>
+    </div>
+    <hr />
+    <section class="menu" :class="{ 'open': isMenuOpen }">
+        <div class="menu-container">
+            <div class="menu-item">
+                <RouterLink class="header-link" to="/" @click="toggleMenu">
+                    <font-awesome-icon :icon="['fa', 'hashtag']" />
+                    Numbers
+                </RouterLink>
+            </div>
+            <div class="menu-item">
+                <RouterLink class="header-link" to="/length" @click="toggleMenu">
+                    <font-awesome-icon :icon="['fa', 'ruler']" />
+                    Lengths
+                </RouterLink>
+            </div>
+            <div class="menu-item">
+                <RouterLink class="header-link" to="/temperature" @click="toggleMenu">
+                    <font-awesome-icon :icon="['fa', 'temperature-half']" />
+                    Temperatures
+                </RouterLink>
+            </div>
+            <div class="menu-item">
+                <RouterLink class="header-link" to="/volume" @click="toggleMenu">
+                    <font-awesome-icon :icon="['fa', 'flask']" />
+                    Volumes
+                </RouterLink>
+            </div>
+            <div class="menu-item">
+                <RouterLink class="header-link" to="/weight" @click="toggleMenu">
+                    <font-awesome-icon :icon="['fa', 'weight-hanging']" />
+                    Weights
+                </RouterLink>
+            </div>
+            <div class="menu-item">
+                <RouterLink class="header-link" to="/time" @click="toggleMenu">
+                    <font-awesome-icon :icon="['fa', 'hourglass']" />
+                    Times
+                </RouterLink>
+            </div>
+            <div class="menu-item">
+                <RouterLink class="header-link" to="/area" @click="toggleMenu">
+                    <font-awesome-icon :icon="['fa', 'arrows-left-right-to-line']" />
+                    Areas
+                </RouterLink>
+            </div>
+        </div>
+    </section>
+    <RouterView />
+    <div class="footer">
+        <p>{{ footer }}</p>
+    </div>
 </template>
-
-<style scoped>
-.hamburger-button {
-	margin: 10px 30px;
-	cursor: pointer;
-}
-
-.bar-1,
-.bar-2,
-.bar-3 {
-	width: 35px;
-	height: 3px;
-	background-color: #a0d2db;
-	margin: 6px 0;
-	transition: 0.4s;
-}
-
-.open>.bar-1 {
-	transform: translate(0, 9px) rotate(-45deg);
-	transition-duration: (100);
-}
-
-.open>.bar-2 {
-	opacity: 0;
-}
-
-.open>.bar-3 {
-	transform: translate(0, -9px) rotate(45deg);
-}
-
-.menu {
-	display: none;
-	z-index: 1;
-	width: 100%;
-	height: calc(100% - 55px);
-	top: 55px;
-	position: fixed;
-	background-color: #2e2e3a;
-}
-
-.menu.open {
-	display: block;
-	/* flex-wrap: wrap; */
-}
-
-.menu-container {
-	display: flex;
-	flex-wrap: wrap;
-	margin: 15px;
-}
-</style>
